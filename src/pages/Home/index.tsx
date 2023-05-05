70e1c7baa9a8bccf1bb3f484e49a47efc4680c14
mclarar <mariac.ribeiro.paula@gmail.com>
2023-05-05 16:47:12 -0300

    refactor: ajuste no estilo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import { series } from "../../data/series";
 import styles from "./Home.module.css";
 import { ResultCard } from "../../components/ResultCard";
 
+import tv from "../../assets/126683-show-time-icon.gif";
+
 export function Home() {
   const [seriado, setSeriado] = useState<Serie>({
     id: 0,
@@ -14,40 +16,43 @@ export function Home() {
     episodios: [],
   });
 
-
-  function randomizarEpisodios(min: number, max: number){
+  function randomizarEpisodios(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-  function randomSeason(min: number, max: number){
+  function randomSeason(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
+
   return (
     <div className={styles.containerApp}>
       <h1 className={styles.title}>Escolha a série que deseja assitir</h1>
       <>
-        {series.map((serie) => {
-          return (
-            <TvShowCard
-              key={serie.id}
-              serie={serie}
-              onCllick={() => {
-                setSeriado(serie);
-                console.log(serie);
-              }}
-            />
-          );
-        })}
+        <div className={styles.cardContainer}>
+          {series.map((serie) => {
+            return (
+              <TvShowCard
+                key={serie.id}
+                serie={serie}
+                onCllick={() => {
+                  setSeriado(serie);
+                  console.log(serie);
+                  
+                }}
+              />
+            );
+          })}
+        </div>
       </>
 
       <ResultCard
         tvshowName={seriado.nome}
-        season={randomSeason(1,10)}
-        episode={randomizarEpisodios(1,20)}
+        season={randomSeason(1, 10)}
+        episode={randomizarEpisodios(1, 20)}
       />
     </div>
   );
